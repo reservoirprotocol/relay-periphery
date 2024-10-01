7f9a126886e98c6052b7cf9005b639af1b9c8436
stephankmin <stephankmin@gmail.com>
2024-10-01 13:44:19 -0400

    chore: rm relay structs import

--- src/OwnableRelayRouter.sol
+++ src/OwnableRelayRouter.sol
@@ -9,8 +9,11 @@ import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ISignatureTransfer} from "permit2-relay/src/interfaces/ISignatureTransfer.sol";
 import {IPermit2} from "permit2-relay/src/interfaces/IPermit2.sol";
-import {RelayerWitness} from "./types/lib/RelayStructs.sol";
 import {Multicaller} from "./utils/Multicaller.sol";
+
+struct RelayerWitness {
+    address relayer;
+}
 
 contract OwnableRelayRouter is Ownable, Multicaller, Tstorish {
     using SafeERC20 for IERC20;
