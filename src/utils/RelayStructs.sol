1bd9aa11d5323985303b76e6b0857c2edbfb2bcc
stephankmin <stephankmin@gmail.com>
2024-12-09 13:41:53 -0500

    chore: update relay structs

--- src/utils/RelayStructs.sol
+++ src/utils/RelayStructs.sol
@@ -1,11 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-contract RelayStructs {
-    struct Call3Value {
-        address target;
-        bool allowFailure;
-        uint256 value;
-        bytes callData;
-    }
+struct Call3Value {
+    address target;
+    bool allowFailure;
+    uint256 value;
+    bytes callData;
+}
+
+struct 
+
+enum ClaimStatus {
+    Initiated__awaitingRelayerResponse,
+    Initiated__awaitingClaimantResponse,
+    EscalatedToArbitration,
+    Cancelled,
+    Settled
 }
