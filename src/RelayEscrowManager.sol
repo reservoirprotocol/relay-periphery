ae47617348f71579b192f11fbc691f57bd213961
stephankmin <stephankmin@gmail.com>
2024-12-11 16:00:08 -0500

    feat: add _initiateClaim

--- src/RelayEscrowManager.sol
+++ src/RelayEscrowManager.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.23;
 import {Ownable} from "solady/src/auth/Ownable.sol";
 import {SignatureCheckerLib} from "solady/src/utils/SignatureCheckerLib.sol";
 import {IRelayEscrowManager} from "./interfaces/IRelayEscrowManager.sol";
-import {ClaimStatus} from "./utils/RelayStructs.sol";
+import {ClaimContext, ClaimStatus} from "./utils/RelayStructs.sol";
 
 /// @title RelayEscrow v1
 /// @notice RelayEscrowManager holds Relayer collateral and allows Users
@@ -13,19 +13,19 @@ import {ClaimStatus} from "./utils/RelayStructs.sol";
 contract EscrowManager is IRelayEscrowManager, Ownable {
     using SignatureCheckerLib for address;
 
-    event ClaimInitiated(address user, address relayer, bytes32 claimId);
-    event ClaimSettled(bytes32 claimId, ClaimStatus claimStatus);
-    event ClaimCancelled(bytes32 claimId);
+    event ClaimInitiated(address user, address relayer, bytes32 commitmentId);
+    event ClaimSettled(bytes32 commitmentId, ClaimStatus claimStatus);
+    event ClaimCancelled(bytes32 commitmentId);
 
     error InvalidSignature(address expectedSigner);
     error InvalidClaimStatus(
-        bytes32 claimId,
+        bytes32 commitmentId,
         ClaimStatus expectedStatus,
         ClaimStatus actualStatus
     );
-    error InsufficientFunds(uint256 amountAvailable, uint256 amountRequested);
-    error ClaimAlreadyInitiated(bytes32 claimId);
-    error ClaimAlreadySettled(bytes32 claimId);
+    error InvalidDisputeBond(uint256 expectedDisputeBond, uint256 msgValue);
+    error ClaimAlreadyInitiated(bytes32 commitmentId);
+    error ClaimAlreadySettled(bytes32 commitmentId);
     error ClaimNotInitiated(bytes32 orderHash);
     error ClaimWindowExpired(uint256 blockTimestamp, uint256 expiration);
     error EtherReturnTransferFailed(
@@ -36,12 +36,15 @@ contract EscrowManager is IRelayEscrowManager, Ownable {
 
     uint96 public claimWindow;
     uint96 public responseWindow;
+    uint96 public arbitrationWindow;
+
+    uint256 public disputeBond;
 
     /// @notice Mapping of relayer balances
     mapping(address => uint256) public balances;
 
-    /// @notice Mapping of order hashes to claim statuses
-    mapping(bytes32 => ClaimStatus) public claimStatus;
+    /// @notice Mapping of commitmentId to claim context
+    mapping(bytes32 => ClaimStatus) public claimContext;
 
     constructor() {
         _initializeOwner(msg.sender);
@@ -52,19 +55,22 @@ contract EscrowManager is IRelayEscrowManager, Ownable {
     /// @notice Deposit collateral on behalf of a relayer
     function depositEscrow(address relayer) public payable {
         // Increment the relayer's total balance
-        balances[relayer] += msg.value;
+        balances[relayer].totalBalance += msg.value;
     }
 
     /// @notice Withdraw collateral from the escrow contract
     /// @param amount The amount to withdraw
     function withdrawEscrow(uint256 amount) public {
+        uint256 withdrawableBalance = balances[msg.sender].totalBalance -
+            balances[msg.sender].outstandingBalance;
+
         // Revert if amount exceeds withdrawable balance
-        if (amount > balances[msg.sender])
-            revert InsufficientFunds(balances[msg.sender], amount);
+        if (amount > withdrawableBalance)
+            revert InsufficientFunds(withdrawableBalance, amount);
 
         unchecked {
             // Decrement the sender's total balance
-            balances[msg.sender] -= amount;
+            balances[msg.sender].totalBalance -= amount;
         }
 
         // Transfer the amount to the relayer
@@ -86,7 +92,7 @@ contract EscrowManager is IRelayEscrowManager, Ownable {
         Commitment commitment,
         bytes32 depositTxHash,
         bytes memory relayerSig
-    ) public returns (bytes32 orderHash) {
+    ) public payable returns (bytes32 orderHash) {
         // Generate the commitment hash
         bytes32 commitmentHash = _getCommitmentHash(commitment);
 
@@ -103,71 +109,65 @@ contract EscrowManager is IRelayEscrowManager, Ownable {
         // Validate that the claim window has not expired
         _validateClaimWindow(commitment.quoteExpiration);
 
-        // Emit an OrderInitiated event
+        // Validate the bond amount
+        _validateBond();
+
+        // Set the claim status to initiated
+        _initiateClaim(commitment.commitmentId);
+
+        // Emit a ClaimInitated event
         emit OrderInitiated(order.user, order.relayer, orderHash);
     }
 
-    /// @notice Settle an order
-    /// @param orderHash The hash of the order to settle
-    /// @param salt The salt used to create the intentCommit
-    /// @param validatorSig The validator's signature
-    /// @param commitData The intentCommit data
-    function settleOrder(
-        bytes32 orderHash,
-        uint256 salt,
-        bytes memory validatorSig,
-        bytes memory commitData
-    ) public {
-        Order memory order = orders[orderHash];
+    // /// @notice Allows an order to be cancelled.
+    // ///         Validator can cancel the order if they determine the User
+    // ///         has not transferred to the Relayer by the order expiration.
+    // function cancelOrder(bytes32 orderHash, bytes memory validatorSig) public {
+    //     Order memory order = orders[orderHash];
 
-        // Check that the order status is initiated
-        if (orderStatus[orderHash] != OrderStatus.Initiated)
-            revert OrderNotInitiated(orderHash);
+    //     // Check that the order status is initiated
+    //     if (orderStatus[orderHash] != OrderStatus.Initiated)
+    //         revert OrderNotInitiated(orderHash);
 
-        // Check that hash of commitData matches commitment
-        if (order.intentCommit != keccak256(abi.encode(commitData, salt)))
-            revert InvalidCommitData(order.intentCommit, commitData, salt);
+    //     // Validate the signature
+    //     if (!order.validator.isValidSignatureNow(orderHash, validatorSig))
+    //         revert InvalidSignature(order.validator);
 
-        // Validate the validator's signature and get the verdict
-        Verdict verdict = _getVerdict(orderHash, validatorSig);
+    //     // Set the order status to cancelled
+    //     orderStatus[orderHash] = OrderStatus.Cancelled;
 
-        // Decode the commitData
-        (, TokenTransfer[] memory outputs) = abi.decode(
-            commitData,
-            (TokenTransfer[], TokenTransfer[])
-        );
+    //     // Update the relayer's outstanding balance
+    //     unchecked {
+    //         balances[order.relayer].outstandingBalance -= order
+    //             .collateralAmount;
+    //     }
 
-        // Settle the order's balances
-        _settleBalances(order, outputs, verdict);
+    //     // Emit an OrderCancelled event
+    //     emit OrderCancelled(order.user, order.relayer, orderHash);
+    // }
 
-        emit OrderSettled(orderHash, verdict);
+    function _initiateClaim(bytes32 commitmentId) internal {
+        // Set the claim status to initiated
+        claimStatus[commitmentId] = ClaimStatus
+            .Initiated__awaitingRelayerResponse;
+
+        // Set the response deadline
+        claimContext[commitmentId].responseDeadline =
+            block.timestamp +
+            responseWindow;
+
+        // Lock msg.sender's dispute bond
+        balances[msg.sender].outstandingBalance += msg.value;
+
+        // Update msg.sender's total balance
+        balances[msg.sender].totalBalance += msg.value;
     }
 
-    /// @notice Allows an order to be cancelled.
-    ///         Validator can cancel the order if they determine the User
-    ///         has not transferred to the Relayer by the order expiration.
-    function cancelOrder(bytes32 orderHash, bytes memory validatorSig) public {
-        Order memory order = orders[orderHash];
-
-        // Check that the order status is initiated
-        if (orderStatus[orderHash] != OrderStatus.Initiated)
-            revert OrderNotInitiated(orderHash);
-
-        // Validate the signature
-        if (!order.validator.isValidSignatureNow(orderHash, validatorSig))
-            revert InvalidSignature(order.validator);
-
-        // Set the order status to cancelled
-        orderStatus[orderHash] = OrderStatus.Cancelled;
-
-        // Update the relayer's outstanding balance
-        unchecked {
-            balances[order.relayer].outstandingBalance -= order
-                .collateralAmount;
+    function _validateDisputeBond() internal {
+        // Validate the dispute bond amount
+        if (msg.value != disputeBond) {
+            revert InvalidDisputeBond(disputeBond, msg.value);
         }
-
-        // Emit an OrderCancelled event
-        emit OrderCancelled(order.user, order.relayer, orderHash);
     }
 
     function _validateRelayerSignature(
@@ -246,89 +246,6 @@ contract EscrowManager is IRelayEscrowManager, Ownable {
                 // Add the amount to the `to` address
                 balances[output.to].relayEthBalance += output.amount;
             }
-        }
-    }
-
-    /// @notice Internal function to initiate an order
-    /// @param order The order to initiate
-    /// @param orderHash The hash of the order
-    function _initiateOrder(Order memory order, bytes32 orderHash) internal {
-        // Check that the order status is NotInitiated
-        if (orderStatus[orderHash] != OrderStatus.NotInitiated)
-            revert OrderAlreadyInitiated(orderHash);
-
-        // Check that the order has not expired
-        if (order.expiration != 0 && block.timestamp > order.expiration)
-            revert OrderExpired(block.timestamp, order.expiration);
-
-        // Add the order to the orders mapping
-        orders[orderHash] = order;
-
-        // Set the order status to initiated
-        orderStatus[orderHash] = OrderStatus.Initiated;
-
-        // Update the relayer's outstanding balance
-        balances[order.relayer].outstandingBalance += order.collateralAmount;
-
-        // Add the order to the relayer's allocated orders
-        relayerOrders[order.relayer].push(order);
-    }
-
-    /// @notice Internal function to validate the validator's signature,
-    ///         update the order status and return the verdict
-    /// @param orderHash The hash of the order
-    /// @param validatorSig The validator's signature
-    function _getVerdict(
-        bytes32 orderHash,
-        bytes memory validatorSig
-    ) internal returns (Verdict verdict) {
-        Order memory order = orders[orderHash];
-
-        // Store the hash the validator must sign for a FulfilledPrimary verdict
-        bytes32 fulfilledPrimaryVerdictHash = keccak256(
-            abi.encodePacked(orderHash, Verdict.FulfilledPrimary)
-        );
-
-        // Store the hash the validator must sign for a FulfilledSecondary verdict
-        bytes32 fulfilledSecondaryVerdictHash = keccak256(
-            abi.encodePacked(orderHash, Verdict.FulfilledSecondary)
-        );
-
-        // Store the hash the validator must sign to refund collateral to the user
-        bytes32 refundVerdictHash = keccak256(
-            abi.encodePacked(orderHash, Verdict.Refund)
-        );
-
-        // If the validator signed a FulfilledPrimary verdict, set the orderStatus to
-        // Settled__FulfilledPrimary and return a FulfilledPrimary verdict
-        if (
-            order.validator.isValidSignatureNow(
-                fulfilledPrimaryVerdictHash,
-                validatorSig
-            )
-        ) {
-            orderStatus[orderHash] = OrderStatus.Settled__FulfilledPrimary;
-            verdict = Verdict.FulfilledPrimary;
-        } else if (
-            // If the validator signed a FulfilledSecondary verdict, set the orderStatus to
-            // Settled__FulfilledSecondary and return a FulfilledSecondary verdict
-            order.validator.isValidSignatureNow(
-                fulfilledSecondaryVerdictHash,
-                validatorSig
-            )
-        ) {
-            orderStatus[orderHash] = OrderStatus.Settled__FulfilledSecondary;
-            verdict = Verdict.FulfilledSecondary;
-        } else if (
-            // If the validator signed a Refund verdict, set the orderStatus to
-            // Settled__Refund and return a Refund verdict
-            order.validator.isValidSignatureNow(refundVerdictHash, validatorSig)
-        ) {
-            orderStatus[orderHash] = OrderStatus.Settled__Refund;
-            verdict = Verdict.Refund;
-        } else {
-            // Revert if the signature did not resolve to a valid verdict
-            revert InvalidSignature(order.validator);
         }
     }
 }
