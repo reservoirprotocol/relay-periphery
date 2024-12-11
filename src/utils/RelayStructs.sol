d1fd12bcdbd6baeeb2713e87c42dd5ef3eef4c77
stephankmin <stephankmin@gmail.com>
2024-12-11 12:19:45 -0500

    fix: update commitment structs

--- src/utils/RelayStructs.sol
+++ src/utils/RelayStructs.sol
@@ -8,12 +8,56 @@ struct Call3Value {
     bytes callData;
 }
 
-struct 
+struct Input {
+    address to;
+    address token;
+    uint256 chainId;
+    uint256 value;
+    uint256 weight;
+    Refund refund;
+}
+
+struct Output {
+    uint256 chainId;
+    address to;
+    address token;
+    uint256 minimumAmount;
+    uint256 expectedAmount;
+    Call3Value[] calls;
+}
+
+struct Refund {
+    uint256 chainId;
+    address to;
+    address token;
+    uint256 minimumAmount;
+}
+
+/// @dev The struct representing a commitment
+/// @param commitmentId The onchain identifier of the commitment
+/// @param relayer The address of the relayer which will insure the request
+/// @param bond The bond amount for this commitment
+/// @param quoteExpiration The timestamp after which the quote is no longer valid
+/// @param salt A random salt value to ensure commitment uniqueness
+/// @param inputs The list of input payments to be sent to the relayer
+/// @param output The output payment to be sent to the user, including a list of calls
+///               to be executed on the destination chain
+struct Commitment {
+    bytes32 commitmentId;
+    address relayer;
+    uint256 bond;
+    uint256 quoteExpiration;
+    bytes32 salt;
+    Input[] inputs;
+    Output output;
+}
 
 enum ClaimStatus {
+    NotInitiated,
     Initiated__awaitingRelayerResponse,
-    Initiated__awaitingClaimantResponse,
+    Initiated__awaitingUserResponse,
     EscalatedToArbitration,
     Cancelled,
-    Settled
+    Settled__forRelayer,
+    Settled__forUser
 }
