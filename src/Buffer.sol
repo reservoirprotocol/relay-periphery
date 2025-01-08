88bb0ecf414640c5ef0cfd5fb9a3a96ec2ca0542
stephankmin <stephankmin@gmail.com>
2025-01-08 14:08:28 -0500

    fix: use SafeTransferLib for eth and erc20 transfer operations

--- src/Buffer.sol
+++ src/Buffer.sol
@@ -15,7 +15,7 @@ struct WithdrawRequest {
 /// @title  RelayCredit
 /// @author Reservoir
 contract RelayCredit {
-    using SafeERC20 for IERC20;
+    using SafeTransferLib for address;
     using SignatureCheckerLib for address;
 
     error InvalidAllocator();
@@ -61,7 +61,7 @@ contract RelayCredit {
     /// @param id The id associated with the transfer
     function depositErc20(address token, uint256 amount, bytes32 id) external {
         // Transfer the tokens to the contract
-        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+        token.safeTransferFrom(msg.sender, address(this), amount);
 
         // Emit the Deposit event
         emit Deposit(token, amount, id);
@@ -80,12 +80,15 @@ contract RelayCredit {
         bytes32 id
     ) external {
         // Transfer the tokens to the contract
-        IERC20(token).safeTransferFrom(from, address(this), amount);
+        token.safeTransferFrom(from, address(this), amount);
 
         // Emit the Deposit event
         emit Deposit(token, amount, id);
     }
 
+    /// @notice Withdraw tokens from the contract with a signed WithdrawRequest from the Allocator
+    /// @param request The WithdrawRequest struct
+    /// @param signature The signature from the Allocator
     function withdraw(
         WithdrawRequest calldata request,
         bytes memory signature
@@ -102,28 +105,10 @@ contract RelayCredit {
 
         // If the token is native, transfer ETH to the recipient
         if (request.token == address(0)) {
-            _send(request.to, request.amount);
+            request.to.safeTransferETH(request.amount);
         } else {
             // Transfer the ERC20 tokens to the recipient
-            IERC20(request.token).safeTransfer(request.to, request.amount);
-        }
-    }
-
-    /// @notice Internal function for transferring ETH
-    /// @param to The recipient address
-    /// @param value The value to send
-    function _send(address to, uint256 value) internal {
-        bool success;
-        assembly {
-            // Save gas by avoiding copying the return data to memory.
-            // Provide at most 100k gas to the internal call, which is
-            // more than enough to cover common use-cases of logic for
-            // receiving native tokens (eg. SCW payable fallbacks).
-            success := call(100000, to, value, 0, 0, 0, 0)
-        }
-
-        if (!success) {
-            revert NativeTransferFailed();
+            request.token.safeTransfer(request.to, request.amount);
         }
     }
 }
