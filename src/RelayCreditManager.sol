73ca1d93886c97c0a696327124fe1e46f6b53a0f
stephankmin <stephankmin@gmail.com>
2025-01-09 11:39:25 -0500

    fix: (last commit removed buffer) add depositor field to functions and event

---
+++ src/RelayCreditManager.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Ownable} from "solady/src/auth/Ownable.sol";
+import {SafeTransferLib} from "solady/src/utils/SafeTransferLib.sol";
+import {SignatureCheckerLib} from "solady/src/utils/SignatureCheckerLib.sol";
+
+struct WithdrawRequest {
+    address token;
+    uint256 amount;
+    address to;
+}
+
+/// @title  RelayCreditManager
+/// @author Reservoir
+contract RelayCreditManager is Ownable {
+    using SafeTransferLib for address;
+    using SignatureCheckerLib for address;
+
+    error InvalidAllocator();
+
+    error InvalidSignature();
+
+    /// @notice Revert if native transfer failed
+    error NativeTransferFailed();
+
+    /// @notice Emit event when a deposit is made
+    event Deposit(address depositor, address token, uint256 value, bytes32 id);
+
+    address public allocator;
+
+    // Emit a Deposit event when native tokens are received
+    receive() external payable {
+        emit Deposit(address(0), msg.value, msg.data);
+    }
+
+    constructor(address _allocator) {
+        allocator = _allocator;
+    }
+
+    /// @notice Set the allocator address
+    /// @param _allocator The new allocator address
+    function setAllocator(address _allocator) external onlyOwner {
+        if (_allocator == address(0)) {
+            revert InvalidAllocator();
+        }
+        allocator = _allocator;
+    }
+
+    /// @notice Deposit native tokens to the contract and emit a Deposit event
+    /// @param id The id associated with the transfer
+    function depositNative(address depositor, bytes32 id) external payable {
+        // Emit the Deposit event
+        emit Deposit(depositor, address(0), msg.value, id);
+    }
+
+    /// @notice Deposit ERC20 token from msg.sender to the contract and emit a Deposit event
+    /// @param token The ERC20 token to transfer
+    /// @param amount The amount to transfer
+    /// @param id The id associated with the transfer
+    function depositErc20(
+        address depositor,
+        address token,
+        uint256 amount,
+        bytes32 id
+    ) external {
+        // Transfer the tokens to the contract
+        token.safeTransferFrom(msg.sender, address(this), amount);
+
+        // Emit the Deposit event
+        emit Deposit(depositor, token, amount, id);
+    }
+
+    /// @notice Withdraw tokens from the contract with a signed WithdrawRequest from the Allocator
+    /// @param request The WithdrawRequest struct
+    /// @param signature The signature from the Allocator
+    function withdraw(
+        WithdrawRequest calldata request,
+        bytes memory signature
+    ) external {
+        // Get the request hash
+        bytes32 requestHash = keccak256(
+            abi.encode(request.token, request.amount, request.to)
+        );
+
+        // Validate the allocator signature
+        if (!allocator.isValidSignatureNow(requestHash, signature)) {
+            revert InvalidSignature();
+        }
+
+        // If the token is native, transfer ETH to the recipient
+        if (request.token == address(0)) {
+            request.to.safeTransferETH(request.amount);
+        } else {
+            // Transfer the ERC20 tokens to the recipient
+            request.token.safeTransfer(request.to, request.amount);
+        }
+    }
+}
