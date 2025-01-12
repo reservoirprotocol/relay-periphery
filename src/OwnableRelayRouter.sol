6b3beb8603e05ada330a3d92f51989067aa5f61c
stephankmin <stephankmin@gmail.com>
2025-01-12 12:13:46 -0500

    chore: rm unused imports

--- src/OwnableRelayRouter.sol
+++ src/OwnableRelayRouter.sol
@@ -2,14 +2,7 @@
 pragma solidity ^0.8.25;
 
 import {Ownable} from "solady/src/auth/Ownable.sol";
-import {Tstorish} from "tstorish/src/Tstorish.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ISignatureTransfer} from "permit2-relay/src/interfaces/ISignatureTransfer.sol";
-import {IPermit2} from "permit2-relay/src/interfaces/IPermit2.sol";
-import {Multicaller} from "./utils/Multicaller.sol";
 
 contract OwnableRelayRouter is RelayRouter, Ownable {
     constructor(address permit2, address owner) RelayRouter(permit2) {
