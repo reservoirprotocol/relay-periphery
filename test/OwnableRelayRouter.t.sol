726fb2a99a177e20890ffa76d75102ef91473027
stephankmin <stephankmin@gmail.com>
2025-01-31 12:45:05 -0500

    tests: add OwnableRelayRouter.t.sol

---
+++ test/OwnableRelayRouter.t.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Test} from "forge-std/Test.sol";
+import {StdUtils} from "forge-std/StdUtils.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IAllowanceHolder} from "0x-settler/src/allowanceholder/IAllowanceHolder.sol";
+import {Permit2} from "permit2-relay/src/Permit2.sol";
+import {ISignatureTransfer} from "permit2-relay/src/interfaces/ISignatureTransfer.sol";
+import {ApprovalProxy} from "../src/v2/ApprovalProxy.sol";
+import {OwnableRelayRouter} from "../src/v2/OwnableRelayRouter.sol";
+import {Call3Value} from "../src/v2/utils/RelayStructs.sol";
+import {NoOpERC20} from "./mocks/NoOpERC20.sol";
+import {TestERC721} from "./mocks/TestERC721.sol";
+import {TestERC721_ERC20PaymentToken} from "./mocks/TestERC721_ERC20PaymentToken.sol";
+import {BaseRelayTest} from "./base/BaseRelayTest.sol";
+import {IUniswapV2Router01} from "./interfaces/IUniswapV2Router02.sol";
+
+contract OwnableRelayRouterTest is Test, BaseRelayTest {
+    Permit2 permit2 = Permit2(0x000000000022D473030F116dDEE9F6B43aC78BA3);
+    OwnableRelayRouter ownableRouter;
+
+    error Unauthorized();
+
+    function setUp() public override {
+        super.setUp();
+
+        ownableRouter = new OwnableRelayRouter(address(permit2), solver.addr);
+    }
+
+    function testMulticall__RevertOnlyOwner() public {
+        Call3Value[] memory calls = new Call3Value[](1);
+        calls[0] = Call3Value({
+            target: address(0),
+            allowFailure: false,
+            value: 0,
+            callData: abi.encodeWithSelector(
+                OwnableRelayRouter.multicall.selector,
+                calls,
+                address(0)
+            )
+        });
+
+        vm.expectRevert(Unauthorized.selector);
+        vm.prank(alice.addr);
+        ownableRouter.multicall(calls, alice.addr);
+    }
+
+    function testPermitMulticall__RevertOnlyOwner() public {
+        ISignatureTransfer.TokenPermissions[]
+            memory permitted = new ISignatureTransfer.TokenPermissions[](1);
+        permitted[0] = ISignatureTransfer.TokenPermissions({
+            token: address(erc20_1),
+            amount: 0.1 ether
+        });
+
+        ISignatureTransfer.PermitBatchTransferFrom
+            memory permit = ISignatureTransfer.PermitBatchTransferFrom({
+                permitted: permitted,
+                nonce: 1,
+                deadline: block.timestamp + 100
+            });
+
+        bytes memory calldata1 = abi.encodeWithSelector(
+            erc20_1.transfer.selector,
+            bob.addr,
+            0.03 ether
+        );
+
+        Call3Value[] memory calls = new Call3Value[](1);
+        calls[0] = Call3Value({
+            target: address(erc20_1),
+            allowFailure: false,
+            value: 0,
+            callData: calldata1
+        });
+
+        vm.expectRevert(Unauthorized.selector);
+        vm.prank(alice.addr);
+        ownableRouter.permitMulticall(alice.addr, permit, calls, "");
+    }
+}
